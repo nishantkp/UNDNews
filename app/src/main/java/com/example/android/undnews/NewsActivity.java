20f38c95723fd0269ffeb82a5aa9d0ed50ea7a75
Nishant Patel <nishantuwindsor@gmail.com>
2017-11-20 22:07:55 -0500

    Add navigation drawer

--- app/src/main/java/com/example/android/undnews/NewsActivity.java
+++ app/src/main/java/com/example/android/undnews/NewsActivity.java
@@ -11,7 +11,11 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -30,7 +34,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NewsActivity extends AppCompatActivity
-        implements LoaderManager.LoaderCallbacks<List<News>> {
+        implements LoaderManager.LoaderCallbacks<List<News>>,
+        NavigationView.OnNavigationItemSelectedListener {
 
     private static final int NEWS_LOADER_ID = 1;
     private static String LOG_TAG = NewsActivity.class.getName();
@@ -59,10 +64,23 @@ public class NewsActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list);
+        setContentView(R.layout.activity_news);
 
         Toolbar toolbar = findViewById(R.id.default_toolbar);
         setSupportActionBar(toolbar);
+
+        // Setup the navigation drawer
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Start the first Menu item in Navigation Drawer and highlight it
+        onNavigationItemSelected(navigationView.getMenu().getItem(0).setChecked(true));
 
         // Find the progress bar with id progress_bar in list.xml
         mProgressBar = findViewById(R.id.progress_bar);
@@ -350,7 +368,7 @@ public class NewsActivity extends AppCompatActivity
     /**
      * This method is used to get the user preference
      */
-    private void getUserPreference(){
+    private void getUserPreference() {
         // Get the preferences provided by user
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         // Author name preference
@@ -367,6 +385,28 @@ public class NewsActivity extends AppCompatActivity
         mOrderByPreference = sharedPreferences.getString(
                 getString(R.string.settings_order_by_key)
                 , getString(R.string.settings_order_by_default_value));
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        //TODO define behaviour of items in navigation view
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
 
