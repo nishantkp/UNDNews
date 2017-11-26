aad18565cffbbf42d747fe1d7ea7792bdae2011b
Nishant Patel <nishantuwindsor@gmail.com>
2017-11-26 00:08:25 -0500

    Display preference summary

--- app/src/main/java/com/example/android/undnews/SettingsActivity.java
+++ app/src/main/java/com/example/android/undnews/SettingsActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.undnews;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,11 +17,45 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
 
-    public static class NewsPreferenceSettings extends PreferenceFragment {
+    public static class NewsPreferenceSettings extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
+
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            /* Find the preference for order by and set the summary */
+            Preference orderByPreference = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderByPreference);
+
+            /* Find preference for number of articles per page and set the summary */
+            Preference numberOfArticlesPreference = findPreference(getString(R.string.settings_number_of_articles_key));
+            bindPreferenceSummaryToValue(numberOfArticlesPreference);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
